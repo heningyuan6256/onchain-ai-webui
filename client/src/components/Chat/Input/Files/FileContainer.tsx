1507773064e5eabc964db8c993164cbf262ab8dc
heningyuan6256 <810151439@qq.com>
2025-11-04 16:54:41 +0800

    修改上传图片的文件

--- client/src/components/Chat/Input/Files/FileContainer.tsx
+++ client/src/components/Chat/Input/Files/FileContainer.tsx
@@ -3,6 +3,8 @@ import type { ExtendedFile } from '~/common';
 import { getFileType, cn } from '~/utils';
 import FilePreview from './FilePreview';
 import RemoveFile from './RemoveFile';
+import Icon from '~/components/icon';
+import FileSvg from "@/assets/image/file.svg";
 
 const FileContainer = ({
   file,
@@ -30,25 +32,29 @@ const FileContainer = ({
         onClick={onClick}
         aria-label={file.filename}
         className={cn(
-          'relative overflow-hidden rounded-2xl border border-border-light bg-surface-hover-alt',
+          'h-[62px] relative overflow-hidden rounded-2xl border border border-[#E0E0E0] bg-white',
           buttonClassName,
         )}
+        style={{ borderRadius: '10px 10px 2px 2px', padding: '10px 10px 18px 10px' }}
       >
-        <div className="w-56 p-1.5">
+        <div className="w-56 p-0 text-xs">
           <div className="flex flex-row items-center gap-2">
-            <FilePreview file={file} fileType={fileType} className="relative" />
+            <Icon className="mr-1.5 w-[28px]" src={FileSvg}></Icon>{" "}
+            {/* <FilePreview file={file} fileType={fileType} className="relative" /> */}
             <div className="overflow-hidden">
               <div className="truncate font-medium" title={file.filename}>
                 {file.filename}
               </div>
-              <div className="truncate text-text-secondary" title={fileType.title}>
-                {fileType.title}
+              <div className="truncate text-[10px] text-[#D0D0D0]" style={{ textAlign: 'left' }} title={"上传完成"}>
+                <span className='text-xs' style={{ transform: 'scale(0.8)', lineHeight: '18px' }}>上传完成</span>
               </div>
             </div>
           </div>
         </div>
       </button>
       {onDelete && <RemoveFile onRemove={onDelete} />}
+      <div className='absolute bottom-0 bg-[#31D780] h-[3px] w-full'>
+      </div>
     </div>
   );
 };
